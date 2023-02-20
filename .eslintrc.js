376701defed7336d42cb36e91c4b876dafabb6ae
Heljä <helja.hietala@gmail.com>
2023-02-20 14:45:15 +0200

    Change babel eslint parser

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,32 +1,32 @@
 module.exports = {
-  parser: 'babel-eslint',
-  env: {
-    browser: true,
-    commonjs: true,
-    es6: true,
-    node: true,
-    jest: true,
-  },
-  parserOptions: {
-    ecmaVersion: 2020,
-    ecmaFeatures: {
-      jsx: true,
+    "parser": "@babel/eslint-parser",
+    env: {
+        browser: true,
+        commonjs: true,
+        es6: true,
+        node: true,
+        jest: true,
     },
-    sourceType: 'module',
-  },
-  plugins: ['react', 'react-hooks'],
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  settings: {
-    react: {
-      version: '16.x',
+    parserOptions: {
+        ecmaVersion: 2020,
+        ecmaFeatures: {
+        jsx: true,
+        },
+        sourceType: 'module',
     },
-  },
-  rules: {
-    'react/prop-types': ['off'],
-    'react/no-unescaped-entities': ['error', { forbid: ['>', '}'] }],
-  },
+    plugins: ['react', 'react-hooks'],
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:react-hooks/recommended',
+    ],
+    settings: {
+        react: {
+        version: '16.x',
+        },
+    },
+    rules: {
+        'react/prop-types': ['off'],
+        'react/no-unescaped-entities': ['error', { forbid: ['>', '}'] }],
+    },
 };
