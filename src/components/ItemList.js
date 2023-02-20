f1ba9d0c144cd882e33b2c206165ee791e22cd16
Heljä <helja.hietala@gmail.com>
2023-02-20 13:50:50 +0200

    Add functionality to add shopping list items and show the added items in the view

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,11 +1,43 @@
 import * as React from 'react';
 
 export default function ItemList() {
-    // const [count, setCount] = React.useState(0);
+    const [items, setItems] = React.useState([]);
+    const [itemName, setItemName] = React.useState();
+
+    const AddItem = (props) => {
+        return (
+            <button
+                className="addButton"
+                onClick={() => props.onClick(props.item)}
+            >
+                ADD
+            </button>
+        );
+    }
+
+    const addNewItem = (item) => {
+        setItems(items.concat(item));
+    }
+
+    const changeItemName = (event) => {
+        setItemName(event.target.value);
+    }
 
     return (
-        <div>
-        <input /> <button>ADD</button>
+        <div style={{display: "flex", flexDirection: "column"}}>
+            <div style={{display: "flex", flexDirection: "row"}}>
+                <input name="itemName" onChange={changeItemName} /> 
+                <br />
+                <AddItem onClick={addNewItem} item={itemName} />
+            </div>
+
+            <br />
+
+            <div style={{display: "flex", flexDirection: "column"}}>
+                {items.map((item, index) => (
+                    <span key={index}>{index + 1}. {item}</span>
+                ))}
+            </div>
         </div>
     );
 }
