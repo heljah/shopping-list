e5d2d738aa7609f4107297e3ace2388822892a4f
Heljä <helja.hietala@gmail.com>
2023-02-20 14:44:31 +0200

    Add delete option to list items

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -2,12 +2,13 @@ import * as React from 'react';
 
 export default function ItemList() {
     const [items, setItems] = React.useState([]);
-    const [itemName, setItemName] = React.useState();
+    const [itemName, setItemName] = React.useState('');
 
     const AddItem = (props) => {
         return (
             <button
                 className="addButton"
+                style={{marginLeft: "2em"}}
                 onClick={() => props.onClick(props.item)}
             >
                 ADD
@@ -15,18 +16,34 @@ export default function ItemList() {
         );
     }
 
-    const addNewItem = (item) => {
-        setItems(items.concat(item));
+    const addNewItem = () => {
+        console.log(itemName);
+        setItems(items.concat(itemName));
+        setItemName('');
     }
 
     const changeItemName = (event) => {
         setItemName(event.target.value);
     }
 
+    const deleteItem = (item_to_delete) => {
+        console.log({item_to_delete, items});
+        setItems(items.filter(item => item != item_to_delete));
+    }
+
+    const ListItemRow = (props) => {
+        return (
+            <span style={{display: "flex", flexDirection: "row"}}>
+                {props.index + 1}. {props.item}
+                <button onClick={() => deleteItem(props.item)} style={{marginLeft: "auto"}}>DELETE</button>
+            </span>
+        )
+    }
+
     return (
-        <div style={{display: "flex", flexDirection: "column"}}>
+        <div style={{display: "flex", flexDirection: "column", width: "max-content"}}>
             <div style={{display: "flex", flexDirection: "row"}}>
-                <input name="itemName" onChange={changeItemName} /> 
+                <input name="itemName" value={itemName} onChange={changeItemName} onKeyDown={(event) => {if (event.key === 'Enter') return addNewItem() }} /> 
                 <br />
                 <AddItem onClick={addNewItem} item={itemName} />
             </div>
@@ -35,7 +52,7 @@ export default function ItemList() {
 
             <div style={{display: "flex", flexDirection: "column"}}>
                 {items.map((item, index) => (
-                    <span key={index}>{index + 1}. {item}</span>
+                    <ListItemRow key={index} index={index} item={item}/>
                 ))}
             </div>
         </div>
