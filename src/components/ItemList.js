1761acc98d2be85cbed4a6be0f0952eca6b4292e
Giovani <giofra.dev@gmail.com>
2023-02-21 15:55:18 +0200

    Edit component added

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -5,6 +5,9 @@ import { solid } from '@fortawesome/fontawesome-svg-core/import.macro' // <-- im
 export default function ItemList() {
     const [items, setItems] = React.useState([]);
     const [itemName, setItemName] = React.useState('');
+    const [showNewInput, setShowNewInput] = React.useState(false);
+    const [newItemName, setNewItemName] = React.useState('');
+    const [indexToBeEdited, setIndexToBeEdited] = React.useState('');
 
     const AddItem = (props) => {
         return (
@@ -14,7 +17,7 @@ export default function ItemList() {
                 onClick={() => props.onClick(props.item)}
             >
                 ADD
-                <FontAwesomeIcon icon={solid('plus')} style={{marginLeft: "2em"}} />
+                <FontAwesomeIcon icon={solid('plus')} style={{marginLeft: "1em"}} />
             </button>
         );
     }
@@ -23,15 +26,38 @@ export default function ItemList() {
         console.log(itemName);
         setItems(items.concat(itemName));
         setItemName('');
+        setShowNewInput(false);
+        setNewItemName('');
     }
 
     const changeItemName = (event) => {
         setItemName(event.target.value);
+        
     }
 
     const deleteItem = (item_to_delete) => {
         console.log({item_to_delete, items});
         setItems(items.filter(item => item != item_to_delete));
+        setShowNewInput(false);
+        setNewItemName('');
+    }
+
+    const editItem = (index) => {
+        setIndexToBeEdited(index);
+        setShowNewInput(true);
+    }
+
+    const changeNewItemName = (e, index) => {
+        console.log(e.target.value, index);
+        setNewItemName(e.target.value);
+    }
+
+    const saveNewItem = (index) => {
+        const newItems = items.slice()
+        newItems[index] = newItemName
+        setItems(newItems);
+        setShowNewInput(false);
+        setNewItemName('');
     }
 
     const ListItemRow = (props) => {
@@ -40,8 +66,10 @@ export default function ItemList() {
                 {props.index + 1}. {props.item}
 
                 <button onClick={() => deleteItem(props.item)} style={{marginLeft: "auto"}}>
-                    DELETE
-                    <FontAwesomeIcon icon={solid('trash')} style={{marginLeft: "2em"}} />
+                    <FontAwesomeIcon icon={solid('trash')} />
+                </button>
+                <button onClick={() => editItem(props.index)} style={{marginLeft: "auto"}}>
+                    <FontAwesomeIcon icon={solid('pen')} />
                 </button>
             </span>
         )
@@ -59,10 +87,18 @@ export default function ItemList() {
 
             <div style={{display: "flex", flexDirection: "column"}}>
                 {items.map((item, index) => (
-                    <ListItemRow key={index} index={index} item={item}/>
-                ))}
+                    <div><ListItemRow key={index} index={index} item={item}/>
+                    {(showNewInput && indexToBeEdited == index) && 
+                    <div>
+                        <input name="newItemName" value={newItemName} onChange={e => changeNewItemName(e,index)}/>
+                        <button onClick={() => saveNewItem(index)} style={{marginLeft: "auto"}}>
+                            <FontAwesomeIcon icon={solid('floppy-disk')} />
+                        </button>
+                    </div>}
+                    </div>
+               
+                ))}       
             </div>
-
         </div>
     );
 }
